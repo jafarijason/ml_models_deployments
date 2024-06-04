7d4e0028599ba62799e2b06d5003d6b418913b59
Jason Jafari <me@jasonjafari.com>
2024-06-04 11:58:00 -0400

    Add fast api to the project

---
+++ main.py
@@ -0,0 +1,19 @@
+import uvicorn
+from typing import Union
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return "Hello Jason"
+
+@app.get("/test")
+def test(name: str , family: str):
+    return f'{family} {name}'
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=5000, log_level="info")
