ed5990f65b18b150c2ed1ced690197b9ecdd5350
Jason Jafari <me@jasonjafari.com>
2024-06-09 00:34:44 -0400

    Added wage and quadratic model for age wage Advance_regression2.ipynb

--- functions/transformers.py
+++ functions/transformers.py
@@ -7,8 +7,14 @@ def MIS_X_GPA(row):
 def GPA_X_Statistics(row):
     return row['GPA'] * row['Statistics']
 
+def AGE_POWER_2(row):
+    return row['Age'] * row['Age']
+
 transformersDict = {
+    # For Advance_regression.ipynb
     "MIS_X_Statistics": MIS_X_Statistics,
     "MIS_X_GPA": MIS_X_GPA,
-    "GPA_X_Statistics": GPA_X_Statistics
+    "GPA_X_Statistics": GPA_X_Statistics,
+    # For Advance_regression2.ipynb
+    "AGE_POWER_2": AGE_POWER_2
 }
