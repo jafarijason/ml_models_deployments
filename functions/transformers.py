b8c531336f21a3b183992342e626d933acdb61d0
Jason Jafari <me@jasonjafari.com>
2024-06-09 01:51:03 -0400

    Add Advance_regression2.ipynb and log transformer fo models

--- functions/transformers.py
+++ functions/transformers.py
@@ -1,3 +1,5 @@
+import math
+
 def MIS_X_Statistics(row):
     return row['MIS'] * row['Statistics']
 
@@ -10,11 +12,16 @@ def GPA_X_Statistics(row):
 def AGE_POWER_2(row):
     return row['Age'] * row['Age']
 
+def Sqft_log(row):
+    return math.log(row['Sqft'])
+
 transformersDict = {
     # For Advance_regression.ipynb
     "MIS_X_Statistics": MIS_X_Statistics,
     "MIS_X_GPA": MIS_X_GPA,
     "GPA_X_Statistics": GPA_X_Statistics,
     # For Advance_regression2.ipynb
-    "AGE_POWER_2": AGE_POWER_2
+    "AGE_POWER_2": AGE_POWER_2,
+    # For Advance_regression3.ipynb
+    "Sqft_log": Sqft_log
 }
